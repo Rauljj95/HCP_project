469948961b9a17b68b21a2ba8c5bdf32dd97fd7e
CyberDany <danielrm1996@gmail.com>
2017-05-01 00:11:04 +0200

    Versión 0.9
    Cambios publicados en la columna terminado.

--- PsicoteXt/PsicoteXt/FormRespuesta.cs
+++ PsicoteXt/PsicoteXt/FormRespuesta.cs
@@ -11,11 +11,6 @@ namespace PsicoteXt
 {
     public partial class FormRespuesta : Form
     {
-        //private int[] ejeXPos;
-        //private int[] ejeXNeg;
-        //private int[] ejeYPos;
-        //private int[] ejeYNeg;
-        //private int dim = 20;
         private int estiloApren; // 1.divergente, 2.adaptador, 3.convergente, 4.asimilador
 
         private int EC; //Experiencia Concreta
@@ -27,8 +22,6 @@ namespace PsicoteXt
         private int y;
 
         enum estilo {Divergente,Adaptador,Convergente,Asimilador};
-
-        
 
         //Métodos Set-------------------------------------------
         public void SetEC(int EC)
@@ -110,93 +103,6 @@ namespace PsicoteXt
             }
         }
         //-------------------------------------------------------
-
-
-        //No se necesita, se ha sustituido por el enum
-
-        //private String NombreEstilo()
-        //{
-        //    switch (estiloApren)
-        //    {
-        //        case 1:
-        //            return "Divergente";
-        //        case 2:
-        //            return "Adaptador";
-        //        case 3:
-        //            return "Convergente";
-        //        case 4:
-        //            return "Asimilador";
-        //        default:
-        //            return " ";
-        //    }
-        //}
-
-        //Código Anterior para calcular los ejes
-
-        //private void InitEjes()
-        //{
-        //    ejeXPos = new int[dim];
-        //    int xPosi = 5; //Valor inicial del eje (5, 4, 3, 2....)
-        //    ejeXNeg = new int[dim];
-        //    int xNegi = 6; //Valor inicial del eje (6, 7, 8, 9....)
-
-        //    ejeYPos = new int[dim];
-        //    int yPosi = 3; //Valor inicial del eje (3, 2, 1, 0....)
-        //    ejeYNeg = new int[dim];
-        //    int yNegi = 4; //Valor inicial del eje (4, 5, 6, 7....)
-
-        //    for (int i = 0; i < dim; i++)
-        //    {
-        //        ejeXPos[i] = xPosi--;
-        //        ejeXNeg[i] = xNegi++;
-        //        ejeYPos[i] = yPosi--;
-        //        ejeYNeg[i] = yNegi++;
-        //    }
-        //}
-        //private void ObtEstilo()
-        //{
-        //    int X = EA-OR;
-        //    int Y = CA-EC;
-        //    bool[] eje = { true, false, false, false };
-
-        //    for (int i= 0; i<dim; i++){
-        //        if (ejeXPos[i] == X)
-        //        {
-        //            eje[0] = true;
-        //            break;
-        //        }
-        //    }
-        //    for (int i=0; i<dim; i++){
-        //        if (ejeYPos[i] == X)
-        //        {
-        //            eje[1] = true;
-        //            break;
-        //        }
-        //    }
-        //    for (int i=0; i<dim; i++){
-        //        if (ejeXNeg[i] == X)
-        //        {
-        //            eje[2] = true;
-        //            break;
-        //        }
-        //    }
-        //    for (int i=0; i<dim; i++){
-        //        if (ejeYNeg[i] == X)
-        //        {
-        //            eje[3] = true;
-        //            break;
-        //        }
-        //    }
-
-        //    if (eje[0] && eje[1])
-        //        estiloApren = 1;
-        //    else if (eje[1] && eje[2])
-        //        estiloApren = 2;
-        //    else if (eje[2] && eje[3])
-        //        estiloApren = 3;
-        //    else
-        //        estiloApren = 4;
-        //}
 
 
         //Inicialización del Form------------------------
---
+++ PsicoteXt/PsicoteXt/MismaPrioridadException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PsicoteXt
+{
+    class MismaPrioridadException : Exception
+    {
+        string mensaje;
+
+        public MismaPrioridadException(string message)
+        {
+            mensaje = message;
+        }
+
+        public string getMensaje()
+        {
+            return mensaje;
+        }
+    }
+}
